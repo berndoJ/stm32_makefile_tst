3e6cf659da6d59bb6fa06d716d438d87290d1f1c
Johannes Berndorfer (berndoJ) <johannesberndorfer82@gmail.com>
2019-12-01 22:54:44 +0100

    Moved SRC and INC folders to new modules.

--- inc/com.hpp
+++
@@ -1,7 +0,0 @@
-#if !defined(__COM_HPP__)
-#define __COM_HPP__
-
-#include "stm32f1xx_hal.h"
-#include "stm32f1xx_hal_conf.h"
-
-#endif
--- inc/main.hpp
+++
@@ -1,8 +0,0 @@
-#if !defined(__MAIN_HPP__)
-#define __MAIN_HPP__
-
-#include "com.hpp"
-
-void _sys_initclk(void);
-
-#endif
